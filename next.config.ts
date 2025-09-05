29b23b47f61da57f51ce53a35c6d200c4f96716b
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-09-05 21:39:15 +0600

    project set upr and all inital package install

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["*"],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
